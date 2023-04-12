08d00d48a0bc83ef3cc2f2b7194d92241caf27f8
Jstorto21 <stortoj@oregonstate.edu>
2023-04-12 16:24:18 -0400

    Removed space

--- average.py
+++ average.py
@@ -10,6 +10,5 @@ num_3 = float(input())
 num_4 = float(input())
 num_5 = float(input())
 average = ((num_1 + num_2 + num_3+ num_4 + num_5)/5)
-
 print("The average of those numbers is:")
 print(average)
