d3aa77957f6a3b91f1e572d702fd35ac0e2489b3
Jstorto21 <stortoj@oregonstate.edu>
2023-04-12 16:14:28 -0400

    New gethub location.

---
+++ average.py
@@ -0,0 +1,14 @@
+# Author: Joe Storto
+# Github Username: Jstorto21
+# Date: 4/12/2023
+# This a program asks for five numbers and then prints out the average of those numbers
+
+print("Please enter five numbers.")
+num_1 = float(input())                              #input or all 5 numbers
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+average = ((num_1 + num_2 + num_3+ num_4 + num_5)//5)  #variable to find total sum of numbers
+
+print("The average of those numbers is:",average)     #print the total sum divided by 5
